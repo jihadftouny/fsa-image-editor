57ac274d6b6923b1095c8c2660e90ce34f0476ec
Gabriel Kanashiro <55795684+kanashir0@users.noreply.github.com>
2020-05-14 23:00:47 -0300

    Added arithmetic functions

--- Multimidia/src/br/fsa/operacoes/Aritmetica.java
+++ Multimidia/src/br/fsa/operacoes/Aritmetica.java
@@ -36,6 +36,97 @@ public class Aritmetica {
 			
 	}
 	
+	public static Imagem substracao(Imagem a, Imagem b){
+		Imagem result = new Imagem(Math.max(a.getW(), b.getW()), Math.max(a.getH(), b.getH()));
+
+		for(int j = 0; j < result.getH(); j++){
+			for(int i = 0; i < result.getW(); i++){
+				int cora = a.getP(i,j);
+				int ra = cora >> 16 & 0xFF;
+				int ga = cora >> 8 & 0xFF;
+				int ba = cora & 0xFF;
+				
+				int corb = b.getP(i,j);
+				int rb = corb >> 16 & 0xFF;
+				int gb = corb >> 8 & 0xFF;
+				int bb = corb & 0xFF;
+
+				int resR = ra - rb < 0 ? 0 : ra - rb;
+				int resG = ga - gb < 0 ? 0 : ga - gb;
+				int resB = ba - bb < 0 ? 0 : ba - bb;
+
+				int corResult = resR << 16 | resG << 8 | resB;
+
+				result.setP(i,j,corResult);
+			}
+		}
+		return result;
+	}
+	
+	public static Imagem multiplicacao(Imagem a, Imagem b){
+		Imagem result = new Imagem(Math.max(a.getW(), b.getW()), Math.max(a.getH(), b.getH()));
+
+		for(int j = 0; j < result.getH(); j++){
+			for(int i = 0; i < result.getW(); i++){
+				int cora = a.getP(i,j);
+				int ra = cora >> 16 & 0xFF;
+				int ga = cora >> 8 & 0xFF;
+				int ba = cora & 0xFF;
+				
+				int corb = b.getP(i,j);
+				int rb = corb >> 16 & 0xFF;
+				int gb = corb >> 8 & 0xFF;
+				int bb = corb & 0xFF;
+
+				int resR = ra * rb > 255 ? 255 : ra * rb;
+				int resG = ga * gb > 255 ? 255 : ga * gb;
+				int resB = ba * bb > 255 ? 255 : ba * bb;
+
+				int corResult = resR << 16 | resG << 8 | resB;
+
+				result.setP(i,j,corResult);
+			}
+		}
+		return result;
+	}
+
+	public static Imagem divisao(Imagem a, Imagem b){
+		Imagem result = new Imagem(Math.max(a.getW(), b.getW()), Math.max(a.getH(), b.getH()));
+
+		for(int j = 0; j < result.getH(); j++){
+			for(int i = 0; i < result.getW(); i++){
+				int cora = a.getP(i,j);
+				int ra = cora >> 16 & 0xFF;
+				int ga = cora >> 8 & 0xFF;
+				int ba = cora & 0xFF;
+				
+				int corb = b.getP(i,j);
+				int rb = corb >> 16 & 0xFF;
+				int gb = corb >> 8 & 0xFF;
+				int bb = corb & 0xFF;
+
+				int resB = 0;
+				int resR = 0;
+				int resG = 0;
+				
+				if(ra != 0 && rb != 0) {
+					resR = ra / rb;
+				}
+				if(ga != 0 && gb != 0) {
+					resG = ga / gb;
+				}
+				if(ba != 0 && bb != 0) {
+					resB = ba / bb;
+				}
+
+				int corResult = resR << 16 | resG << 8 | resB;
+
+				result.setP(i,j,corResult);
+			}
+		}
+		return result;
+	}
+	
 	public static Imagem soma(Imagem a, int cor) {
 		Imagem result = new Imagem(a.getW(), a.getH());
 		int r = cor >> 16 & 0xff;
