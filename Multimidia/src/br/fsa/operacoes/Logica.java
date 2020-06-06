525b4c2e6591c75f04e0d10434845fd581d905a5
Gabriel Kanashiro <55795684+kanashir0@users.noreply.github.com>
2020-06-06 15:04:55 -0300

    Implemented logical bitwise operators

--- Multimidia/src/br/fsa/operacoes/Logica.java
+++ Multimidia/src/br/fsa/operacoes/Logica.java
@@ -8,11 +8,13 @@ public class Logica {
 		Imagem result = new Imagem(Math.max(a.getW(),  b.getW()), Math.max(a.getH(), b.getH()));
 		for(int j = 0; j < result.getH(); j++) {
 			for(int i = 0; i < result.getW(); i++) {
+				
 				//Separa os canais de cor de A
 				int cora = a.getP(i, j);
 				int ra = cora >> 16 & 0xff;
 				int ga = cora >> 8 & 0xff;
 				int ba = cora & 0xff;
+				
 				//Separa os canais de cor de B
 				int corb = b.getP(i, j);
 				int rb = corb >> 16 & 0xff;
@@ -33,7 +35,90 @@ public class Logica {
 		return result;
 	}
 	
-	//public static Imagem and(Imagem a, int cor) {
+	public static Imagem or(Imagem a, Imagem b) {
+		Imagem result = new Imagem(Math.max(a.getW(),  b.getW()), Math.max(a.getH(), b.getH()));
+		for(int j = 0; j < result.getH(); j++) {
+			for(int i = 0; i < result.getW(); i++) {
+				
+				//Separa os canais de cor de A
+				int cora = a.getP(i, j);
+				int ra = cora >> 16 & 0xff;
+				int ga = cora >> 8 & 0xff;
+				int ba = cora & 0xff;
+				
+				//Separa os canais de cor de B
+				int corb = b.getP(i, j);
+				int rb = corb >> 16 & 0xff;
+				int gb = corb >> 8 & 0xff;
+				int bb = corb & 0xff;
+				
+				//Pra cada canal faz Ra | Rb
+				int resR = ra | rb;
+				int resG = ga | gb;
+				int resB = ba | bb;
+				
+				//Junta os resultados e salva o pixel na imagem resultante
+				int corR = resR << 16 | resG << 8 | resB;
+				result.setP(i, j, corR);
+			}
+		}
 		
-	//}
+		return result;
+	}
+	
+	public static Imagem xor(Imagem a, Imagem b) {
+		Imagem result = new Imagem(Math.max(a.getW(),  b.getW()), Math.max(a.getH(), b.getH()));
+		for(int j = 0; j < result.getH(); j++) {
+			for(int i = 0; i < result.getW(); i++) {
+				
+				//Separa os canais de cor de A
+				int cora = a.getP(i, j);
+				int ra = cora >> 16 & 0xff;
+				int ga = cora >> 8 & 0xff;
+				int ba = cora & 0xff;
+				
+				//Separa os canais de cor de B
+				int corb = b.getP(i, j);
+				int rb = corb >> 16 & 0xff;
+				int gb = corb >> 8 & 0xff;
+				int bb = corb & 0xff;
+				
+				//Pra cada canal faz Ra ^ Rb
+				int resR = ra ^ rb;
+				int resG = ga ^ gb;
+				int resB = ba ^ bb;
+				
+				//Junta os resultados e salva o pixel na imagem resultante
+				int corR = resR << 16 | resG << 8 | resB;
+				result.setP(i, j, corR);
+			}
+		}
+		
+		return result;
+	}
+	
+	public static Imagem not(Imagem a) {
+		Imagem result = new Imagem(a.getW(), a.getH());
+		for(int j = 0; j < result.getH(); j++) {
+			for(int i = 0; i < result.getW(); i++) {
+				
+				//Separa os canais de cor de A
+				int cora = a.getP(i, j);
+				int ra = cora >> 16 & 0xff;
+				int ga = cora >> 8 & 0xff;
+				int ba = cora & 0xff;
+				
+				//Pra cada canal faz ~Ra 
+				int resR = ~ra;
+				int resG = ~ga;
+				int resB = ~ba;
+				
+				//Junta os resultados e salva o pixel na imagem resultante
+				int corR = resR << 16 | resG << 8 | resB;
+				result.setP(i, j, corR);
+			}
+		}
+		
+		return result;
+	}
 }
